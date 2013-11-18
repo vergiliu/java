99732647858df93b281b97fcfdef5a85d99380dc
vergiliu <vergiliu@gmail.com>
2013-11-18 14:09:27 +0100

    ch 19 - added setup

--- TDD/python/test.py
+++ TDD/python/test.py
@@ -2,25 +2,44 @@ class TestCase():
     def __init__(self, a_name):
         self.name = a_name
 
+    def setup(self):
+        pass
+
     def run(self):
+        self.setup()
         method = getattr(self, self.name)
         method()
 
 
 class TestCaseTest(TestCase):
+    def __init__(self, a_name):
+        self.test = None
+        TestCase.__init__(self, a_name)
+
+    def setup(self):
+        self.test = WasRun("test_method")
+
+    def test_setup(self):
+        self.test.run()
+        assert self.test.was_setup
+
     def test_running(self):
-        test = WasRun("test_method")
-        assert not test.was_run
-        test.run()
-        assert test.was_run
+        self.test.run()
+        assert self.test.was_run
 
 
 class WasRun(TestCase):
     def __init__(self, a_name):
         self.was_run = None
+        self.was_setup = None
         TestCase.__init__(self, a_name)
+
+    def setup(self):
+        self.was_run = 1
+        self.was_setup = 1
 
     def test_method(self):
         self.was_run = 1
+
 
 TestCaseTest("test_running").run()
