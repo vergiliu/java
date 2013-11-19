8615c4e2958420149c467ef79bf9c66c8ec3d3f1
vergiliu <vergiliu@gmail.com>
2013-11-19 11:02:11 +0100

    added teardown, logging all steps

--- TDD/python/test.py
+++ TDD/python/test.py
@@ -9,37 +9,41 @@ class TestCase():
         self.setup()
         method = getattr(self, self.name)
         method()
+        self.tear_down()
+
+    def tear_down(self):
+        pass
 
 
 class TestCaseTest(TestCase):
-    def __init__(self, a_name):
-        self.test = None
-        TestCase.__init__(self, a_name)
-
     def setup(self):
         self.test = WasRun("test_method")
 
-    def test_setup(self):
-        self.test.run()
-        assert self.test.was_setup
+    def template_method(self):
+        test = WasRun("test_method")
+        test.run()
+        assert ("setup test_method tear_down " == test.log)
 
-    def test_running(self):
-        self.test.run()
-        assert self.test.was_run
+    def tear_down(self):
+        pass
 
 
 class WasRun(TestCase):
     def __init__(self, a_name):
-        self.was_run = None
-        self.was_setup = None
-        TestCase.__init__(self, a_name)
+        self.name = a_name
+        self.log = None
 
     def setup(self):
-        self.was_run = 1
+        self.was_run = None
         self.was_setup = 1
+        self.log = "setup "
 
     def test_method(self):
         self.was_run = 1
+        self.log += "test_method "
 
+    def tear_down(self):
+        self.log += "tear_down "
 
-TestCaseTest("test_running").run()
+if __name__ == "__main__":
+    TestCaseTest("template_method").run()
