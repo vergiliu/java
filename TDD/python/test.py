f5f4b7830e2cfe24aafa5bac8c423e10070539a8
vergiliu <vergiliu@gmail.com>
2013-11-21 11:34:22 +0100

    ch 23 implementing a test suite

--- TDD/python/test.py
+++ TDD/python/test.py
@@ -5,17 +5,16 @@ class TestCase():
     def setup(self):
         pass
 
-    def run(self):
-        result = TestResult()
-        result.test_started()
+    def run(self, a_result):
+        # removed extra init :)
+        a_result.test_started()
         self.setup()
         try:
             method = getattr(self, self.name)
             method()
         except:
-            result.test_failed()
+            a_result.test_failed()
         self.tear_down()
-        return result
 
     def tear_down(self):
         pass
@@ -24,30 +23,37 @@ class TestCase():
 class TestCaseTest(TestCase):
     def setup(self):
         self.test = WasRun("test_method")
+        self.result = TestResult()
+
+    def test_suite(self):
+        suite = TestSuite()
+        suite.add(WasRun("test_method"))
+        suite.add(WasRun("broken_test"))
+        suite.run(self.result)
+        assert("2 run, 1 failed" == self.result.summary())
 
     def template_method(self):
         test = WasRun("test_method")
-        test.run()
+        test.run(self.result)
         assert ("setup test_method tear_down " == test.log)
 
     def fail_test(self):
         test = WasRun("broken_test")
-        result = test.run()
-        assert("1 run, 1 failed" == result.summary())
+        test.run(self.result)
+        assert("1 run, 1 failed" == self.result.summary())
 
     def tear_down(self):
         pass
 
     def test_result(self):
         test = WasRun("test_method")
-        result = test.run()
-        assert("1 run, 0 failed" == result.summary())
+        test.run(self.result)
+        assert("1 run, 0 failed" == self.result.summary())
 
     def failer_result_formatting(self):
-        result = TestResult()
-        result.test_started()
-        result.test_failed()
-        assert("1 run, 1 failed" == result.summary())
+        self.result.test_started()
+        self.result.test_failed()
+        assert("1 run, 1 failed" == self.result.summary())
 
 
 class TestResult:
@@ -64,6 +70,16 @@ class TestResult:
     def summary(self):
         return "%d run, %d failed" % (self.run_count, self.error_count)
 
+class TestSuite:
+    def __init__(self):
+        self.tests = []
+
+    def add(self, test):
+        self.tests.append(test)
+
+    def run(self, a_result):
+        for test in self.tests:
+            test.run(a_result)
 
 class WasRun(TestCase):
     def __init__(self, a_name):
@@ -86,7 +102,11 @@ class WasRun(TestCase):
         self.log += "tear_down "
 
 if __name__ == "__main__":
-    print(TestCaseTest("template_method").run().summary())
-    print(TestCaseTest("test_result").run().summary())
-    print(TestCaseTest("fail_test").run().summary())
-    print(TestCaseTest("failer_result_formatting").run().summary())
+    suite = TestSuite()
+    suite.add(TestCaseTest("template_method"))
+    suite.add(TestCaseTest("fail_test"))
+    suite.add(TestCaseTest("test_suite"))
+    suite.add(TestCaseTest("failure_result_formatting"))
+    end_result = TestResult()
+    suite.run(end_result)
+    print(end_result.summary())
