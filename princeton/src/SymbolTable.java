db405226b6d3f860719a26e55e7d899a9623b3c4
vergiliu <vergiliu@gmail.com>
2014-08-10 16:23:58 +0200

    symbol table API initial

---
+++ princeton/src/SymbolTable.java
@@ -0,0 +1,37 @@
+
+public class SymbolTable<Key, Value> {
+    public SymbolTable() {}
+
+    public void put(Key key, Value value) {
+
+    }
+
+    public Value get(Key key) {
+        //TODO
+        return null;
+    }
+
+    public void delete(Key key) {
+        //TODO
+    }
+
+    public boolean contains(Key key) {
+        //TODO
+        return false;
+    }
+
+    public boolean isEmpty() {
+        //TODO
+        return true;
+    }
+
+    public int size() {
+        //TODO
+        return 0;
+    }
+
+    public Iterable<Key> keys() {
+        //TODO
+        return null;
+    }
+}
