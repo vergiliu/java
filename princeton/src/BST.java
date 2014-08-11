26ffb03953f242f462c222cf8710881ea5b48772
vergiliu <vergiliu@gmail.com>
2014-08-11 20:40:44 +0200

    updated BST w/ size, needed for ranking

--- princeton/src/BST.java
+++ princeton/src/BST.java
@@ -1,7 +1,9 @@
 
 public class BST<Key extends Comparable<Key>, Value> {
+    private Node root;
 
     private class Node {
+        private int count;
         private Key key;
         private Value value;
         private Node left, right;
@@ -11,11 +13,38 @@ public class BST<Key extends Comparable<Key>, Value> {
         }
     }
 
-    public void put(Key key, Value value) {
-        //TODO
+    public int size() {
+        return size(root);
     }
 
+    private int size(Node node) {
+        if (node == null) return 0;
+        return node.count;
+    }
+
+    public void put(Key key, Value value) {
+        root = put(root, key, value);
+    }
+
+    private Node put(Node node, Key key, Value value) {
+        if (node == null) return new Node(key, value);
+        int direction = key.compareTo(node.key);
+        if (direction < 0) node.left = put(node.left, key, value);
+        else if (direction > 0) node.right = put(node.right, key, value);
+        else node.value = value;
+        node.count = 1 + size(node.left) + size(node.right);
+        return node;
+    }
+
+
     public Value get(Key key) {
+        Node n = root;
+        while (n != null) {
+            int direction = key.compareTo(n.key);
+            if (direction < 0) n = n.left;
+            else if (direction > 0) n = n.right;
+            else return n.value;
+        }
         return null;
     }
 
